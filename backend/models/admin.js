f6d71bcefee0f6118af677afdc070c9a2d53fdd3
kriti009 <kriti.ritu.dew@gmail.com>
2019-09-11 14:43:16 +0530

    deleting extras

--- backend/models/admin.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-
-var adminSchema = new mongoose.Schema({
-    username : {type: String},
-    password : {type: String},
-    jwtToken: [{type:String}],
-});
-module.exports = mongoose.model("Admin", adminSchema);
--- backend/models/product.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-var productSchema = new mongoose.Schema({
-    type: {type: String},
-    price : {type: Number}, //per kg
-    amount : {type: Number}, // in kg
-});
-
-module.exports = mongoose.model("Product", productSchema);
